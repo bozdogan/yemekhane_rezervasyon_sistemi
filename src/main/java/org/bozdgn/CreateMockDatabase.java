feeffe96ffdcc270efe568d69dff8d1b7199f3ec
Bozdogan <boraozdogan99@gmail.com>
2022-05-01 01:57:37 +0300

    separate data from driver script

--- src/main/java/org/bozdgn/CreateMockDatabase.java
+++ src/main/java/org/bozdgn/CreateMockDatabase.java
@@ -1,18 +1,34 @@
 package org.bozdgn;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.*;
+import java.util.List;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 public class CreateMockDatabase {
     static String username = "refectory";
     static String password = "";
     static String dbUrl = "jdbc:mysql://localhost/refectory";
 
-    static void runMultipleStatements(
-            Connection conn,
-            String queryString
-    ) throws SQLException {
+    static String queryDropAllTables = "MockData_Nuke.sql";
+    static String queryCreateTables = "MockData_CreateTables.sql";
+    static String queryPopulateTables = "MockData_Populate.sql";
+
+    static void runSQLBatch(Connection conn, String resourceLocator) throws SQLException{
+        InputStream resourceStream = CreateMockDatabase.class.
+                getClassLoader().getResourceAsStream(resourceLocator);
+        if(resourceStream == null) {
+            System.out.printf("Cannot read resource `%s`\n", resourceLocator);
+            return;
+        }
+
+        BufferedReader r = new BufferedReader(new InputStreamReader(resourceStream));
+        String queryString = r.lines().collect(Collectors.joining("\n"));
+
         for(String q: queryString.split(";")) {
             String query = q.trim();
             if(query.length() != 0) {
@@ -24,7 +40,7 @@ public class CreateMockDatabase {
         }
     }
 
-    public static void main(String[] args) throws SQLException, IOException {
+    public static void main(String[] args) throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver"); // assert JDBC driver
         }
@@ -32,6 +48,7 @@ public class CreateMockDatabase {
             System.out.println("MySQL driver not found");
             return;
         }
+
 
         Properties dbInfo = new Properties();
         dbInfo.setProperty("user", username);
@@ -44,131 +61,11 @@ public class CreateMockDatabase {
         try(Connection conn = DriverManager.getConnection(dbUrl, dbInfo)) {
 
             // NOTE(bora): Reset database
-            runMultipleStatements(conn, queryDropAllTables);
-            runMultipleStatements(conn, queryCreateTables);
+            runSQLBatch(conn, queryDropAllTables);
+            runSQLBatch(conn, queryCreateTables);
 
             // NOTE(bora): Populate tables
-            runMultipleStatements(conn, queryPopulateTables);
+            runSQLBatch(conn, queryPopulateTables);
         }
     }
-
-
-    private static final String queryDropAllTables = ""
-        + "SET FOREIGN_KEY_CHECKS=0;\n"
-        + "DROP TABLE IF EXISTS `users`;\n"
-        + "DROP TABLE IF EXISTS `meal`;\n"
-        + "DROP TABLE IF EXISTS `food`;\n"
-        + "DROP TABLE IF EXISTS `reservations`;\n"
-        + "DROP TABLE IF EXISTS `purchase`;\n"
-        + "DROP TABLE IF EXISTS `has_food`;\n"
-        + "DROP TABLE IF EXISTS `has_meal`;\n"
-        + "SET FOREIGN_KEY_CHECKS=1;" ;
-
-    private static final String queryCreateTables = ""
-        + "CREATE TABLE `users` (\n"
-        + "    `pid` int NOT NULL,\n"
-        + "    `firstname` varchar(50) NOT NULL,\n"
-        + "    `lastname` varchar(50) NOT NULL,\n"
-        + "    `password` varchar(50) NOT NULL,\n"
-        + "    PRIMARY KEY (`pid`)\n"
-        + ")\n"
-        + "COLLATE='utf8_turkish_ci'\n"
-        + "ENGINE=InnoDB\n"
-        + ";\n"
-        + "\n"
-        + "\n"
-        + "CREATE TABLE `meal` (\n"
-        + "    `mid` INT(10) NOT NULL AUTO_INCREMENT,\n"
-        + "    `date` DATE NOT NULL,\n"
-        + "    `repast` ENUM('B','L','D') NOT NULL,\n"
-        + "    PRIMARY KEY (`mid`)\n"
-        + ")\n"
-        + "COLLATE='utf8_turkish_ci'\n"
-        + "ENGINE=InnoDB\n"
-        + "AUTO_INCREMENT=2\n"
-        + ";\n"
-        + "\n"
-        + "\n"
-        + "CREATE TABLE `food` (\n"
-        + "    `fid` INT(10) NOT NULL AUTO_INCREMENT,\n"
-        + "    `food_name` VARCHAR(50) NOT NULL,\n"
-        + "    PRIMARY KEY (`fid`)\n"
-        + ")\n"
-        + "COLLATE='utf8_turkish_ci'\n"
-        + "ENGINE=InnoDB\n"
-        + "AUTO_INCREMENT=11\n"
-        + ";\n"
-        + "\n"
-        + "\n"
-        + "CREATE TABLE `reservations` (\n"
-        + "    `pid` INT(10) NOT NULL,\n"
-        + "    `mid` INT(10) NOT NULL,\n"
-        + "    `refectory` ENUM('ieylul','yemre') NOT NULL,\n"
-        + "    PRIMARY KEY (`pid`, `mid`),\n"
-        + "    INDEX `FK_reservations_meal` (`mid`),\n"
-        + "    CONSTRAINT `FK_reservations_meal` FOREIGN KEY (`mid`) REFERENCES `meal` (`mid`) ON UPDATE CASCADE ON DELETE RESTRICT,\n"
-        + "    CONSTRAINT `FK_reservations_users` FOREIGN KEY (`pid`) REFERENCES `users` (`pid`) ON UPDATE CASCADE ON DELETE RESTRICT\n"
-        + ")\n"
-        + "COLLATE='utf8_turkish_ci'\n"
-        + "ENGINE=InnoDB\n"
-        + ";\n"
-        + "\n"
-        + "\n"
-        + "CREATE TABLE `purchase` (\n"
-        + "    `pid` INT(10) NOT NULL,\n"
-        + "    `mid` INT(10) NOT NULL,\n"
-        + "    `date` DATE NOT NULL,\n"
-        + "    `repast` ENUM('B','L','D') NOT NULL,\n"
-        + "    `refectory` ENUM('ieylul','yemre') NOT NULL,\n"
-        + "    PRIMARY KEY (`pid`, `mid`, `date`),\n"
-        + "    INDEX `FK_purchase_meal` (`mid`),\n"
-        + "    CONSTRAINT `FK_purchase_meal` FOREIGN KEY (`mid`) REFERENCES `meal` (`mid`) ON UPDATE CASCADE ON DELETE RESTRICT,\n"
-        + "    CONSTRAINT `FK_purchase_users` FOREIGN KEY (`pid`) REFERENCES `users` (`pid`) ON UPDATE CASCADE ON DELETE RESTRICT\n"
-        + ")\n"
-        + "COLLATE='utf8_turkish_ci'\n"
-        + "ENGINE=InnoDB\n"
-        + ";\n"
-        + "\n"
-        + "\n"
-        + "CREATE TABLE `has_food` (\n"
-        + "    `fid` INT(10) NOT NULL,\n"
-        + "    `mid` INT(10) NOT NULL,\n"
-        + "    PRIMARY KEY (`fid`, `mid`)\n"
-        + ")\n"
-        + "COLLATE='utf8_turkish_ci'\n"
-        + "ENGINE=InnoDB\n"
-        + ";\n"
-        + "\n"
-        + "\n"
-        + "CREATE TABLE `has_meal` (\n"
-        + "    `pid` INT(10) NOT NULL,\n"
-        + "    `mid` INT(10) NOT NULL,\n"
-        + "    `refectory` ENUM('ieylul','yemre') NOT NULL,\n"
-        + "    PRIMARY KEY (`pid`, `mid`)\n"
-        + ")\n"
-        + "COLLATE='utf8_turkish_ci'\n"
-        + "ENGINE=InnoDB\n"
-        + ";" ;
-
-    private static final String queryPopulateTables = ""
-        + "INSERT INTO `food` (`fid`, `food_name`) VALUES\n"
-        + "    (11, 'Patlıcan'),\n"
-        + "    (12, 'Mercimek Çorbası'),\n"
-        + "    (13, 'Pirinç Pilav'),\n"
-        + "    (14, 'Puding'),\n"
-        + "    (15, 'Poğaça'),\n"
-        + "    (16, 'Haşlanmış Yumurta'),\n"
-        + "    (17, 'Simit'),\n"
-        + "    (18, 'Patates Yemeği'),\n"
-        + "    (19, 'Karışık Kızartma'),\n"
-        + "    (20, 'Orman Kebabı'),\n"
-        + "    (21, 'Keşkül'),\n"
-        + "    (22, 'Bulgur Pilavı'),\n"
-        + "    (23, 'Yeşil Mercimek'),\n"
-        + "    (24, 'Bulgur Pilavı');"
-        + "\n"
-        + "INSERT INTO `users` (`pid`, `firstname`, `lastname`, `password`) VALUES\n"
-        + "    (10000101, 'Can', 'Simit', '1234'),\n"
-        + "    (10000102, 'Ayşe', 'Fatma', '1234'),\n"
-        + "    (10000103, 'Ahmet', 'Mehmet', '123');\n";
 }
---
+++ src/main/resources/MockData_Populate.sql
@@ -0,0 +1,25 @@
+
+
+INSERT INTO `food` (`fid`, `food_name`) VALUES
+    (11, 'Patlıcan'),
+    (12, 'Mercimek Çorbası'),
+    (13, 'Pirinç Pilav'),
+    (14, 'Puding'),
+    (15, 'Poğaça'),
+    (16, 'Haşlanmış Yumurta'),
+    (17, 'Simit'),
+    (18, 'Patates Yemeği'),
+    (19, 'Karışık Kızartma'),
+    (20, 'Orman Kebabı'),
+    (21, 'Keşkül'),
+    (22, 'Bulgur Pilavı'),
+    (23, 'Yeşil Mercimek'),
+    (24, 'Bulgur Pilavı');
+
+
+INSERT INTO `users` (`pid`, `firstname`, `lastname`, `password`) VALUES
+    (10000101, 'Can', 'Simit', '1234'),
+    (10000102, 'Ayşe', 'Fatma', '1234'),
+    (10000103, 'Ahmet', 'Mehmet', '123');
+
+
